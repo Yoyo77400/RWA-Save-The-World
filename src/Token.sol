1c12a0f9a3ddfcebc4d7bc7e0b10e159fa483d41
Yoyo77400 <yohan.georgelin@gmail.com>
2025-03-19 16:13:24 +0100

    oppen zeppelin installation

--- src/Token.sol
+++ src/Token.sol
@@ -1,3 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) Ownable(_msgSender()) {
+
+    }
+
+    function mint (address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+    
+}
